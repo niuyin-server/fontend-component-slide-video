765b258528191c6e6f94f7d7b125bdcd9bed6a6c
roydon <3133010060@qq.com>
2024-12-16 17:19:37 +0800

    更换新的视频播放组件

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -43,8 +43,18 @@ const props = defineProps({
 const options = reactive({
   i18n: {
     speed: '速度',
-    normal: '正常'
-  }
+    normal: '正常',
+    quality: '清晰度'
+  },
+  settings: ['captions', 'quality', 'speed', 'loop'],
+  speed: {
+    selected: 1.0,
+    options: [0.75, 1.0, 1.25, 1.5, 2.0, 3.0]
+  },
+  quality: {
+    default: 1080,
+    options: [4320, 2880, 2160, 1440, 1080, 720]
+  },
 })
 </script>
 
@@ -54,6 +64,7 @@ const options = reactive({
   <vue-plyr :options="options">
     <video controls crossorigin playsinline :data-poster="video.coverImage">
       <source size="1080" :src="video.videoUrl" type="video/mp4"/>
+      <source size="1440" :src="video.videoUrl" type="video/mp4"/>
     </video>
   </vue-plyr>
   <img :src="video.coverImage" alt="" class="slide-video-box--blur-cover"/>
